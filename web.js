e99b4551e56317b9291e9826ba96cd9dea92ed4e
OpenShift guest <viesturs.meikulans@gmail.com>
2013-07-10 13:41:39 -0400

    part 1 implemented

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(content());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var content = function() {
+    return fs.readFileSync("index.html", "UTF-8");
+}
